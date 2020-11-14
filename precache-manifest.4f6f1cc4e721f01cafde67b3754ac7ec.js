b741bd500dd2c822246d2a45e693bca68e23d73d
Muhammad Shahryar <64373780+shahrya@users.noreply.github.com>
2020-11-15 00:51:15 +0500

    Add files via upload

---
+++ precache-manifest.4f6f1cc4e721f01cafde67b3754ac7ec.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f7f841ea6edf1912407c4459549a51a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5ea8188e4884c2f68f6c",
+    "url": "/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "42e4bfed4b6e0d034f86",
+    "url": "/static/css/main.d55a36bb.chunk.css"
+  },
+  {
+    "revision": "5ea8188e4884c2f68f6c",
+    "url": "/static/js/2.6bedde37.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/static/js/2.6bedde37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42e4bfed4b6e0d034f86",
+    "url": "/static/js/main.3a71c513.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  },
+  {
+    "revision": "c2464e6f89c2caf36b19d0548be79604",
+    "url": "/static/media/Slide.c2464e6f.JPG"
+  },
+  {
+    "revision": "fd5e0637c7fa0d73dcb034ce34ac3250",
+    "url": "/static/media/card1.fd5e0637.jpg"
+  },
+  {
+    "revision": "acdc9db42de439f04283f41a57cabe0d",
+    "url": "/static/media/card10.acdc9db4.jpg"
+  },
+  {
+    "revision": "7a14f9001d81ec33713de63010b93025",
+    "url": "/static/media/card11.7a14f900.jpg"
+  },
+  {
+    "revision": "b3159e3544f53a6a09fda23000a6187b",
+    "url": "/static/media/card12.b3159e35.jpg"
+  },
+  {
+    "revision": "6e2ebe60e6d52d6526b9b0191634f1a7",
+    "url": "/static/media/card13.6e2ebe60.jpg"
+  },
+  {
+    "revision": "e79e9e7415a0377fce5294c422c6ea65",
+    "url": "/static/media/card14.e79e9e74.jpg"
+  },
+  {
+    "revision": "ead65fa0217fa8a5809a689320f67791",
+    "url": "/static/media/card15.ead65fa0.jpg"
+  },
+  {
+    "revision": "11849b70eca2fd2911813292e56bce2b",
+    "url": "/static/media/card16.11849b70.jpg"
+  },
+  {
+    "revision": "e2ce9fe2b626352e481403350907ddbb",
+    "url": "/static/media/card17.e2ce9fe2.jpg"
+  },
+  {
+    "revision": "0aa98daa7c3db4565306ffdc96e31713",
+    "url": "/static/media/card18.0aa98daa.jpg"
+  },
+  {
+    "revision": "96b65d58952586ad7217400d4dc5746f",
+    "url": "/static/media/card19.96b65d58.jpg"
+  },
+  {
+    "revision": "f7bc7b5fc8b97906856583c8f5428d48",
+    "url": "/static/media/card2.f7bc7b5f.jpg"
+  },
+  {
+    "revision": "d0d957014ed7384ede3c0c770dd4af78",
+    "url": "/static/media/card20.d0d95701.jpg"
+  },
+  {
+    "revision": "6d577d4c427aa8c88e0b85086228a169",
+    "url": "/static/media/card21.6d577d4c.jpg"
+  },
+  {
+    "revision": "47b58951dbf515831655937549620d24",
+    "url": "/static/media/card22.47b58951.jpg"
+  },
+  {
+    "revision": "144e198f5a32577db0d4324c8afd63c4",
+    "url": "/static/media/card23.144e198f.jpg"
+  },
+  {
+    "revision": "5ace7dd25955ce194cd77c24f696acd4",
+    "url": "/static/media/card24.5ace7dd2.jpg"
+  },
+  {
+    "revision": "40520d9d1ca7fa7fa345f284e557e2cf",
+    "url": "/static/media/card25.40520d9d.jpg"
+  },
+  {
+    "revision": "54905cfb0998c4f6d556e5959c63257a",
+    "url": "/static/media/card26.54905cfb.jpg"
+  },
+  {
+    "revision": "2fade3dfbc63a91c8552ccbb882de0de",
+    "url": "/static/media/card27.2fade3df.jpg"
+  },
+  {
+    "revision": "74c6a1e616d85b7f3a0b49cb8f7a02a5",
+    "url": "/static/media/card28.74c6a1e6.jpg"
+  },
+  {
+    "revision": "2d32eaf90ac7472ae9a5adc42806ef44",
+    "url": "/static/media/card3.2d32eaf9.jpg"
+  },
+  {
+    "revision": "cef7985cd8137c93a0a8a36d384b3c73",
+    "url": "/static/media/card4.cef7985c.jpg"
+  },
+  {
+    "revision": "93898dac7bd09bc6250b5aaae9835261",
+    "url": "/static/media/card5.93898dac.jpg"
+  },
+  {
+    "revision": "0e275f7a68be08c2e3e9fe33ff865bbe",
+    "url": "/static/media/card6.0e275f7a.jpg"
+  },
+  {
+    "revision": "e9c4e2e7927be9508c3fde7463fa8a9a",
+    "url": "/static/media/card7.e9c4e2e7.jpg"
+  },
+  {
+    "revision": "01b61f6d529b2ad7f5e66ecdbbfbf979",
+    "url": "/static/media/card8.01b61f6d.jpg"
+  },
+  {
+    "revision": "fe3a348ca00bf2c106b171b11d11f100",
+    "url": "/static/media/card9.fe3a348c.jpg"
+  },
+  {
+    "revision": "d11d084f7ba3ccd15889d0efa27a8521",
+    "url": "/static/media/logo.d11d084f.JPG"
+  }
+]);
